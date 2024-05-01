1ca9cbe1717ec350ca848f74a44471750b1f5e75
sanjay641 <sanjaythapa9808@gmail.com>
2024-05-01 21:49:51 +0545

    setting the cors allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,6 +26,10 @@ SECRET_KEY = 'django-insecure-$e1ln97&dl@)-j$aurekm$yhorn*oh-wchb!@*us%axls_te+6
 DEBUG = True
 
 ALLOWED_HOSTS = []
+CORS_ALLOWED_ORIGINS ={
+    "http://localhost:5173/",
+    "http://127.0.0.1:5173/"
+}
 
 
 # Application definition
@@ -37,11 +41,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #  External Apps
+    'rest_framework',
+    'corsheaders',
+    #   Internal Apps
+    'todo',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
