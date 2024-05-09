c4801f246d4f3be3d5b238b6349f99a4c8223e88
sanjay641 <sanjaythapa9808@gmail.com>
2024-05-09 17:14:07 +0545

    creating components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,9 @@ function App() {
 
   return (
     <div className='bg-indigo-100 min-h-screen'>
-      <h1 className='text-5xl text-center'>Hello world</h1>
+      <nav className='pt-8'>
+      <h1 className='text-5xl text-center'>Todo List</h1>
+      </nav>
 
     </div>
   )
