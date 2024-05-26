f30c22df2feda6622894c2c18cebf1890d511952
sanjay641 <sanjaythapa9808@gmail.com>
2024-05-26 12:32:28 +0545

    adding table

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -2,6 +2,30 @@ import React from 'react'
 
 export const Table = () => {
   return (
-    <div>Tables</div>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Checkbox</th>
+            <th>To Do</th>
+            <th>Status</th>
+            <th>Data Created</th>
+            <th>Actions</th>
+
+
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>True</th>
+            <th>Task1</th>
+            <th>done</th>
+            <th>22-08-2024</th>
+            <th>Action</th>
+
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
