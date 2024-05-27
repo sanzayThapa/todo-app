b9044c831ef158502cdde33b60be1612608886e9
sanjay641 <sanjaythapa9808@gmail.com>
2024-05-27 18:11:58 +0545

    CRUP functionality

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,40 +1,51 @@
 import axios from 'axios';
-import React from 'react';
-import { MdOutlineDeleteSweep, MdCheckBoxOutlineBlank, MdCheckBox, MdOutlineCheckBox, MdEditNote, MdOutlineCheckBoxOutlineBlank  } from "react-icons/md";
+import React, { useState } from 'react';
+import { MdOutlineDeleteSweep, MdOutlineCheckBox, MdOutlineCheckBoxOutlineBlank, MdEditNote } from "react-icons/md";
 
-export const Table = ({todo, setTodo, isLoading}) => {
+export const Table = ({ todo, setTodo, isLoading }) => {
+  const [editText, setEditText] = useState({
+    id: null,
+    body: ''
+  });
 
   const handleDelete = async (id) => {
     try {
       await axios.delete(`http://localhost:8000/api/todo/${id}/`);
-      const newList = todo.filter(todo => todo.id !== id)
-      setTodo(newList)
+      const newList = todo.filter(todoItem => todoItem.id !== id);
+      setTodo(newList);
     } catch (error) {
       console.log(error);
     }
   };
-  
-  const handleEdit = async (id,value) =>{
-    try{
-      const response = await axios.patch(`http://localhost:8000/api/todo/${id}/`, value)
-      const newTodo = todo.map(todo => todo.id === id ? response.data : todo)
-      setTodo(newTodo)
 
-    }catch(error){
-      console.log(error)
+  const handleEdit = async (id, value) => {
+    try {
+      const response = await axios.patch(`http://localhost:8000/api/todo/${id}/`, value);
+      const newTodo = todo.map(todoItem => todoItem.id === id ? response.data : todoItem);
+      setTodo(newTodo);
+    } catch (error) {
+      console.log(error);
     }
-
   };
-  const handleCheckbox = (id,value) =>{
-    handleEdit(id,{
-      'completed': !value
-    })
-  }
 
+  const handleCheckbox = (id, value) => {
+    handleEdit(id, { 'completed': !value });
+  };
 
+  const handleChange = (e) => {
+    setEditText(prev => ({
+      ...prev,
+      'body': e.target.value
+    }));
+  };
 
-
-
+  const handleClick = () => {
+    handleEdit(editText.id, editText);
+    setEditText({
+      id: null,
+      'body': ''
+    });
+  };
 
   return (
     <div className='py-8 flex justify-center'>
@@ -57,7 +68,9 @@ export const Table = ({todo, setTodo, isLoading}) => {
             todo.map((todoItem) => (
               <tr key={todoItem.id} className='border-b border-black'>
                 <td className='p-3 text-sm' title={todoItem.id}>
-                  <span onClick={() => handleCheckbox(todoItem.id,todoItem.completed)} className='inline-block cursor-pointer'>{todoItem.completed ? <MdOutlineCheckBox/> : <MdOutlineCheckBoxOutlineBlank/>}</span>
+                  <span onClick={() => handleCheckbox(todoItem.id, todoItem.completed)} className='inline-block cursor-pointer'>
+                    {todoItem.completed ? <MdOutlineCheckBox /> : <MdOutlineCheckBoxOutlineBlank />}
+                  </span>
                 </td>
                 <td className='p-3 text-sm'>{todoItem.body}</td>
                 <td className='p-3 text-sm text-center'>
@@ -67,14 +80,34 @@ export const Table = ({todo, setTodo, isLoading}) => {
                 </td>
                 <td className='p-3 text-sm'>{new Date(todoItem.created).toLocaleString()}</td>
                 <td className='p-3 text-sm font-medium grid grid-flow-col items-center mt-5'>
-                  <span className='text-xl cursor-pointer'><MdEditNote/></span>
-                  <span className='text-xl inline-block cursor-pointer'><MdOutlineDeleteSweep onClick={ () => handleDelete(todoItem.id)}/></span>
+                  <span className='text-xl cursor-pointer'>
+                    <label htmlFor="my_modal_6" className="btn" onClick={() => setEditText(todoItem)}><MdEditNote /></label>
+                  </span>
+                  <span className='text-xl inline-block cursor-pointer'><MdOutlineDeleteSweep onClick={() => handleDelete(todoItem.id)} /></span>
                 </td>
               </tr>
             ))
           )}
         </tbody>
       </table>
+
+      <input type="checkbox" id="my_modal_6" className="modal-toggle" />
+      <div className="modal" role="dialog">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Edit Todo</h3>
+          <input
+            type="text"
+            placeholder="Type here"
+            value={editText.body}
+            onChange={handleChange}
+            className="input input-bordered w-full max-w-xs mt-8"
+          />
+          <div className="modal-action">
+            <label htmlFor="my_modal_6" onClick={handleClick} className="btn btn-primary">Edit</label>
+            <label htmlFor="my_modal_6" className="btn">Close</label>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
