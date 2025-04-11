84c345d181a54a59a478f42035c90d8c2372563d
sanzayThapa <sanjaythapa9808@gmail.com>
2025-04-11 11:18:00 +0545

    updated color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
   return (
     <div className='bg-indigo-100 min-h-screen'>
       <nav className='pt-8'>
-      <h1 className='text-5xl text-center pb-10'>Todo Lists</h1>
+      <h1 className='text-6xl text-center pb-10'>Todo Lists</h1>
       </nav>
       <TodoForm
       setTodo={setTodo}
